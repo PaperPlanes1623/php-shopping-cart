91ea47d5ff392c716779c87d3c793d5996cc6e99
Dylan Taft <dylbronjames@gmail.com>
2020-06-08 23:54:57 -0600

    created DB mysql file

---
+++ CreateDB.php
@@ -0,0 +1,55 @@
+<?php
+
+class CreateDB 
+{
+  public $servername;
+  public $username;
+  public $password;
+  public $dbname;
+  public $tablename;
+  public $con;
+
+  //constructor
+  public function __construct(
+    $dbname = "NewDB",
+    $tablename = "Productdb",
+    $servername = "localhost",
+    $username = "root",
+    $password=""
+  ){
+    $this->dbname = $dbname;
+    $this->tablename = $tablename;
+    $this->servername = $servername;
+    $this->password = $password;
+
+    //create connection
+    $this->con = mysqli_connect($servername, $username, $password);
+
+    //check connection
+    if(!$this->con){
+      die("Connection Failed: " . mysqli_connect_error());
+    }
+
+    //query
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+    //execute query
+    if(mysqli_query($this->con, $sql)){
+      $this->con = mysqli_connect($servername, $username, $password, $dbname);
+
+      //sql to create table
+      $sql = "CREATE TABLE IF NOT EXISTS $tablename
+      (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      product_name VARCHAR(25) NOT NULL,
+      product_price FLOAT,
+      product_image VARCHAR(100)
+      );";
+
+      if(!mysqli_query($this->con, $sql)){
+        echo "Error creating table:" . mysqli_error($this->con);
+      }
+    } else {
+      return false;
+    }
+  }
+}
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+require_once('CreateDB.php');
+require_once('component.php');
+
+//create instance of CreateDB class
+$database = new CreateDB("Productdb", "Producttb");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,106 +26,12 @@
 
 <div class="container">
   <div class="row text-center py-5">
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-      <form action="index.php" method="post">
-        <div class="card shadow">
-          <div>
-            <img src="product1.jpg" alt="image1" class="img-fluid card-img-top">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Product1</h5>
-            <h6>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="far fa-star"></i>
-            </h6>
-            <p class="card-text">Some quick example text to build on the card</p>
-            <h5>
-            <small><s class="text-secondary">$615</s></small>
-              <span class="price">$599</span>
-            </h5>
-            <button type="submit" name="add" class="btn btn-warning my-3">Add to Cart<i class="fas fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-    <form action="index.php" method="post">
-        <div class="card shadow">
-          <div>
-            <img src="product1.jpg" alt="image1" class="img-fluid card-img-top">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Product1</h5>
-            <h6>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="far fa-star"></i>
-            </h6>
-            <p class="card-text">Some quick example text to build on the card</p>
-            <h5>
-            <small><s class="text-secondary">$615</s></small>
-              <span class="price">$599</span>
-            </h5>
-            <button type="submit" name="add" class="btn btn-warning my-3">Add to Cart<i class="fas fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-    <form action="index.php" method="post">
-        <div class="card shadow">
-          <div>
-            <img src="product1.jpg" alt="image1" class="img-fluid card-img-top">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Product1</h5>
-            <h6>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="far fa-star"></i>
-            </h6>
-            <p class="card-text">Some quick example text to build on the card</p>
-            <h5>
-            <small><s class="text-secondary">$615</s></small>
-              <span class="price">$599</span>
-            </h5>
-            <button type="submit" name="add" class="btn btn-warning my-3">Add to Cart<i class="fas fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="col-md-3 col-sm-6 my-3 my-md-0">
-    <form action="index.php" method="post">
-        <div class="card shadow">
-          <div>
-            <img src="product1.jpg" alt="image1" class="img-fluid card-img-top">
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Product1</h5>
-            <h6>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="far fa-star"></i>
-            </h6>
-            <p class="card-text">Some quick example text to build on the card</p>
-            <h5>
-            <small><s class="text-secondary">$615</s></small>
-              <span class="price">$599</span>
-            </h5>
-            <button type="submit" name="add" class="btn btn-warning my-3">Add to Cart<i class="fas fa-shopping-cart"></i></button>
-          </div>
-        </div>
-      </form>
-    </div>
+   <?php 
+    component("Product1", "$599", "$615", "product1.jpg");
+    component("Product2", "$219", "$300", "product1.jpg");
+    component("Product3", "$1000", "$1150", "product1.jpg");
+    component("Product4", "$80", "$95", "product1.jpg");
+   ?>
   </div>
 </div>
   
